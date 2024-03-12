54291f5bcd651274f083b1e74829ff822e551684
MFJess <jessica2001mf@gmail.com.>
2024-03-12 16:10:12 +0000

    Add tpc1.

---
+++ tpc1/manifesto.md
@@ -0,0 +1,6 @@
+# TPC1 - Opcional
+Durante a primeira aula foi feito um *script* que processa um texto de um livro e devolve a frequência de cada palavra ocorrente.
+Foi ainda adicionada uma opção "-m" que quando usada no formato "-m x", em que x é um inteiro, devolve apenas as x palavras com maior frequência.
+
+Foi-nos sugerido como tpc adicionar duas outras *flags* como por exemplo:
+- uma opção "-n" para apresentar a informação em ordem alfabética e não numérica;
---
+++ tpc1/word_freq.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+'''
+NAME
+   word_freq - Calculates the word frequency in a text
+
+SYNOPSIS
+   word_freq [options] input_files
+   options: 
+        -m 20 : Shows the 20th most frequent words
+        -n : Order alfabetically
+   
+Description'''
+
+from jjcli import * 
+from collections import Counter
+import sys
+import re
+
+cl=clfilter("nm:", doc=__doc__)
+print(cl.opt)
+def tokaniza(texto):
+    palavras = re.findall(r'\w+(?:\-\w+)?|[.,?!;:—]+', texto)
+    return palavras
+
+def imprime(lista):
+    for palavra, n_ocorr in lista:
+        print(f"{palavra} --> {n_ocorr}")
+        
+for txt in cl.text(): 
+    lista_palavras = tokaniza(txt)
+    ocorr = Counter(lista_palavras)
+    
+    if "-n" in cl.opt and "-m" in cl.opt:
+        imprime(sorted(ocorr.most_common(int(cl.opt.get("-m")))))
+    
+    elif "-m" in cl.opt:
+        imprime(ocorr.most_common(int(cl.opt.get("-m"))))
+
+    elif "-n" in cl.opt:
+        imprime(sorted(ocorr.items()))
+    
+    
+
+    else:
+        imprime(ocorr.items())
+
+
+
